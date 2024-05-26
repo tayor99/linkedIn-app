636ea9ab845b9551da93666760180f8718716d50
Tayor99 <adetayo@flanceapp.com>
2024-05-26 23:26:54 +0100

    first commit

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,34 +1,83 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
-
-import { TabBarIcon } from '@/components/navigation/TabBarIcon';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { View, Text } from "react-native";
+import React from "react";
+import { Tabs } from "expo-router";
+import { TabBarIcon } from "@/components/navigation/TabBarIcon";
+import { Entypo, MaterialIcons } from "@expo/vector-icons";
+import { FontAwesome } from "@expo/vector-icons";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+        // tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
         headerShown: false,
-      }}>
+        tabBarLabelStyle: { fontSize: 12, fontWeight: 600 },
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
+          title: "Home",
           tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
+            <Entypo
+              name="home"
+              size={28}
+              color="grey"
+              style={{ marginBottom: -3 }}
+            />
           ),
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="myNetwork"
         options={{
-          title: 'Explore',
+          title: "My Network",
           tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
+            <MaterialIcons
+              name="people-alt"
+              size={28}
+              color="grey"
+              style={{ marginBottom: -3 }}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="post"
+        options={{
+          title: "Post",
+          tabBarIcon: ({ color, focused }) => (
+            <Entypo
+              name="squared-plus"
+              size={28}
+              color="grey"
+              style={{ marginBottom: -3 }}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="notifications"
+        options={{
+          title: "Notifications",
+          tabBarIcon: ({ color, focused }) => (
+            <TabBarIcon name={"notifications"} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="jobs"
+        options={{
+          title: "Jobs",
+
+          tabBarIcon: ({ color, focused }) => (
+            <FontAwesome
+              name="briefcase"
+              size={28}
+              color="grey"
+              style={{ marginBottom: -3 }}
+            />
           ),
         }}
       />
